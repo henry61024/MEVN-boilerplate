96cc6f55fa824ad5e9ab294293b2d3f6254a3d69
henry61024 <henry35208@hotmail.com.tw>
2018-05-16 00:40:41 +0800

    [feature] add run.sh

---
+++ run.sh
@@ -0,0 +1,17 @@
+dirname=$(pwd)
+
+function buildClient() {
+    echo "===== Building client production ====="
+    cd ${dirname}/client
+    npm run build
+}
+
+function startServer() {
+    echo "===== Starting server ====="
+    cd ${dirname}/server
+    npm start
+}
+
+buildClient
+startServer
+echo "===== Server started!! ====="
